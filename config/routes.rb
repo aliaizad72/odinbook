77cbbe86bd4ffc524e06ef495c54381713a1a0af
Ali Aizad <aliaizad72@gmail.com>
2024-09-12 17:54:52 +0800

    Styled the login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Defines the root path route ("/")
   root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
