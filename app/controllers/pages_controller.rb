b090676275789dc1e6783fa67c9a325df41ce829
Ali Aizad <aliaizad72@gmail.com>
2024-09-14 16:39:50 +0800

    Add placeholder design for Post create form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     if user_signed_in?
+      @user = User.find_by(id: current_user.id)
+      @posts = @user.posts
     else
       redirect_to new_user_session_path
     end
