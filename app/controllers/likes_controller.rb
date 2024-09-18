cc31903ea8e5b676643584b1b677412df36628fb
Ali Aizad <aliaizad72@gmail.com>
2024-09-18 15:09:48 +0800

    Users can like and unlike posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,4 +4,11 @@ class LikesController < ApplicationController
     @post.liked_users << current_user
     redirect_back_or_to root_path
   end
+
+  def unlike
+    @post = Post.find_by(id: params[:id])
+    @like = Like.find_by(post_id: @post.id, user_id: current_user.id)
+    @like.destroy
+    redirect_back_or_to root_path
+  end
 end
