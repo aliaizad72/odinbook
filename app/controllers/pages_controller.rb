3dc011a93642c4ecb1f61773ffee1b4c3fea05e5
Ali Aizad <aliaizad72@gmail.com>
2024-09-19 01:27:35 +0800

    Abstract users list and styled some pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     if user_signed_in?
       @posts = (current_user.followings.map(&:posts).flatten + current_user.posts).sort_by(&:created_at).reverse!
       @post = Post.new
-      @users = User.where.not(id: current_user.id)
+      @users = User.where.not(id: current_user.id).filter { |user| current_user.followings.exclude?(user) }
     else
       redirect_to new_user_session_path
     end
