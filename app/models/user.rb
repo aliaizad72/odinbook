d1fb9dfa57839af06c3306757a2c098729a58dde
Ali Aizad <aliaizad72@gmail.com>
2024-09-22 17:20:43 +0800

    Users can upload profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
+  has_one_attached :avatar
+
   has_many :posts, dependent: :destroy
 
   has_many :comments
