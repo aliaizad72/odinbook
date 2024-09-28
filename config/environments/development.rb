590c8c1fcfdae4e60680d62032324d922da30e25
Ali Aizad <aliaizad72@gmail.com>
2024-09-28 17:26:32 +0800

    Fix safari red tab color

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << ENV["RAILWAY_PUBLIC_DOMAIN"]
+  # config.hosts << ENV["RAILWAY_PUBLIC_DOMAIN"]
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -36,11 +36,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # setting up letter_opener
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         "smtp.gmail.com",
+    port:            587,
+    domain:          "http://127.0.0.1:3000",
+    user_name:       "aliaizad72@gmail.com",
+    password:        "kmglkdmzubmyrbcp",
+    authentication:  "plain",
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
